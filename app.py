1d98651a50752c8d873963b606527766131200c0
isaam-him <isaam@isaam-n.bahid-aui>
2025-10-19 12:59:38 +0100

    Made the app work on the local network

--- app.py
+++ app.py
@@ -635,5 +635,5 @@ def mark_notification_seen(notif_id):
     return jsonify(success=True)
 # Start server
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
-    #app.run(host="0.0.0.0", port=8080, debug=True)
+    #app.run(port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
